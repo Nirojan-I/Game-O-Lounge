9e54a513aaba50a65b287e1a844df60095f0718e
Nirojan <100482954+Nirojan-I@users.noreply.github.com>
2022-04-17 10:50:47 +0530

    Add files via upload

--- signup.php
+++ signup.php
@@ -13,8 +13,14 @@ die("Connection Failed: ".mysqli_connect_error());
 mysqli_query($conn,"INSERT INTO score VALUES('$_POST[username]','Memory Game','30')");
 mysqli_query($conn,"INSERT INTO score VALUES('$_POST[username]','2D Breakout Game','0')");
 mysqli_query($conn,"INSERT INTO score VALUES('$_POST[username]','Space Invaders','0')");
+
 $result=mysqli_query($conn,"SELECT * FROM sign_up");
 $count=1;
+if(!mysqli_num_rows($result)){
+mysqli_query($conn,"INSERT INTO sign_up VALUES('Admin','nirojani.1217@gmail.com','admin','admin','000000','NULL')");
+}
+$result=mysqli_query($conn,"SELECT * FROM sign_up");
+if(mysqli_num_rows($result)){
 while($row = mysqli_fetch_assoc($result))
 {
 $token=rand(100000,900000);
@@ -25,6 +31,7 @@ if(!mysqli_query($conn,$sql)){
 	die("Error: ".mysqli_error($conn));
 }
 header("Location: login page.html");
+}
 }
 }
 mysqli_close($conn);
